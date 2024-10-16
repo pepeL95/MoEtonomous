fb1117c015dfdf86eba24ce191eb05a5b930cd60
pepeL95 <jose.lopezgarrido95@gmail.com>
2024-10-16 15:21:17 -0400

    react agent refactor

--- JB007/base/ephemeral_react_agent.py
+++ JB007/base/ephemeral_react_agent.py
@@ -28,73 +28,39 @@ class EphemeralReactAgent(Agent):
         # Init agent
         self._make_agent()
 
-################################################### GETTERS #####################################################
-    
-    @property
-    def llm(self):
-        return self._llm
-
-    @property
-    def prompt_template(self):
-        return self._prompt_template
-    
-    @property
-    def system_prompt(self):
-        return self._system_prompt
-    
-    @property
-    def parser(self):
-        return self._parser
-    
-    @property
-    def name(self):
-        return self._name
-    
-################################################## SETTERS #####################################################
-
-    @llm.setter
-    def llm(self, llm):
-        self._llm = llm
-        self._make_agent()
-
-    @prompt_template.setter
-    def prompt_template(self, prompt_template):
-        self._prompt_template = prompt_template
-        self._make_agent()
-    
-    @system_prompt.setter
-    def system_prompt(self, system_prompt):
-        self._system_prompt = system_prompt
-        self._make_agent()
-
-    @parser.setter
-    def parser(self, parser):
-        self._parser = parser
-        self._make_agent()
-
 ######################################## PRIVATE METHODS #########################################################
 
     def _make_agent(self):
         '''Create a react_agent using Langchain.'''
-        if self._system_prompt is None and self._prompt_template is None:
-            prompt = hub.pull("hwchase17/react")
+        if self._system_prompt is None:
+            prompt = self._prompt_template or hub.pull("hwchase17/react")
             self._agent = create_react_agent(self._llm, self._tools, prompt)
             return
         
-        if self._prompt_template is not None:
-            prompt = PromptTemplate.from_template(self._prompt_template)
+        if self._system_prompt is not None:
+            # Get template
+            human_template = ("human", self._prompt_template or hub.pull("hwchase17/react").template)
+            # Build prompt
+            prompt = ChatPromptTemplate.from_messages([
+                    ("system", self._system_prompt),
+                    human_template
+                    ])
+            # Build agent
             self._agent = create_react_agent(self._llm, self._tools, prompt)
             return
-        
-        # Get template
-        human_template = ("human", hub.pull("hwchase17/react").template)
-        # Build prompt
-        prompt = ChatPromptTemplate.from_messages([
-                ("system", self._system_prompt),
-                human_template
-                ])
-        # Build agent
-        self._agent = create_react_agent(self._llm, self._tools, prompt)
+
+    def _invoke_with_prompt_template(self, input, stream):
+        agent_executor = AgentExecutor(agent=self._agent, tools=self._tools, verbose=self._verbose, handle_parsing_errors=True)
+        if self._parser is not None:
+            agent_executor = (
+                agent_executor
+                | RunnableLambda(lambda response: response["output"]) | self._parser
+            )
+        return agent_executor.invoke(input)
+    
+    def _invoke_without_prompt_template(self, input, stream):
+        # There is always a prompt template, by design
+        return self._invoke_with_prompt_template(input, stream)
 
 ############################################# PUBLIC METHODS ####################################################
 
