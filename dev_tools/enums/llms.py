62cd7eed8f8814c10e67b960516ffbe7a34457a8
Jose Lopez <98781642+pepeL95@users.noreply.github.com>
2024-10-16 21:09:26 -0400

    updated llms.py

---
+++ dev_tools/enums/llms.py
@@ -0,0 +1,67 @@
+import os
+
+from langchain_community.llms import LlamaCpp
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+class LLMs:
+
+    @staticmethod
+    def PHI_3():
+        phi = LlamaCpp(
+            model_path=os.environ.get('MODEL_PATH_PHI_3_IT_Q4_GGUF'),
+            n_gpu_layers=-1,
+            n_batch=1024,
+            n_ctx=4096,
+            temperature=0,
+            max_tokens=2000,
+            f16_kv=True,
+            verbose=False,
+        )
+        return phi
+    @staticmethod
+    def PHI_35():
+        phi = LlamaCpp(
+            model_path=os.environ.get('MODEL_PATH_PHI_35_IT_Q4_GGUF'),
+            n_gpu_layers=-1,
+            n_batch=1024,
+            n_ctx=8192,
+            temperature=0,
+            max_tokens=2000,
+            f16_kv=True,
+            verbose=False,
+        )
+        return phi
+    
+
+    @staticmethod
+    def BIG_GEMMA():
+        google = LlamaCpp(
+            model_path=os.environ.get('MODEL_PATH_GEMMA_9B_IT_Q4_GGUF'),
+            n_gpu_layers=-1,
+            n_batch=1024,
+            n_ctx=8192,
+            temperature=0,
+            max_tokens=2000,
+            f16_kv=True,
+            verbose=False,
+        )
+        return google
+    
+    @staticmethod
+    def MINI_GEMMA():
+        google = LlamaCpp(
+            model_path=os.environ.get('MODEL_PATH_GEMMA_2B_IT_Q4_GGUF'),
+            n_gpu_layers=-1,
+            n_batch=1024,
+            n_ctx=8192,
+            temperature=0,
+            max_tokens=2000,
+            f16_kv=True,
+            verbose=False,
+        )
+        return google
+    
+    @staticmethod
+    def GEMINI():
+        gemini = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0)
+        return gemini
