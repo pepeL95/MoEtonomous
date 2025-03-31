5115e8eeb312db7a20b7c6ec22e89c9001032a99
pepeL95 <jose.lopezgarrido95@gmail.com>
2025-03-30 23:21:51 -0400

    added documentation to mdtree

--- dev_tools/utils/mdtree.py
+++ dev_tools/utils/mdtree.py
@@ -23,11 +23,11 @@ class MarkdownTree:
         md_splits = md_splitter.split_text(md_str)
         if not 'Title' in md_splits[0].metadata:
             md_splits = md_splitter.split_text(f"# Document\n\n{md_str}")
-        
+
         tree = MarkdownTree()
         tree._build_document_tree(md_chunks=md_splits)
         return tree
-    
+
     @staticmethod
     def from_langdocs(md_chunks: List[Document]):
         tree = MarkdownTree()
@@ -39,72 +39,93 @@ class MarkdownTree:
 
         # Set root
         self.root = md_chunks[0]
+        self.root.metadata['parent'] = None
+        self.root.metadata["level"] = 1
         self.root.metadata['summary'] = None
         self.root.metadata['id'] = self.root.metadata['Title']
 
         # Vertices of the tree
         self.V = {self.root.metadata["id"]: self.root}
 
-        for doc_split in md_chunks:
+        for current_document in md_chunks:
             _self, parent, grandparent = None, None, None
 
             # Create children if not exists
-            if not "children" in doc_split.metadata:
-                doc_split.metadata["children"] = []
+            if not "children" in current_document.metadata:
+                current_document.metadata["children"] = []
 
             # <H4/>
-            if "Detailed Subsection" in doc_split.metadata:
+            if "Detailed Subsection" in current_document.metadata:
                 _self = "Detailed Subsection"
                 parent = "Subsection"
                 grandparent = "Section"
+                current_document.metadata["level"] = 4
 
             # <H3/>
-            elif "Subsection" in doc_split.metadata:
+            elif "Subsection" in current_document.metadata:
                 _self = "Subsection"
                 parent = "Section"
                 grandparent = "Title"
+                current_document.metadata["level"] = 3
 
             # <H2/>
-            elif "Section" in doc_split.metadata:
+            elif "Section" in current_document.metadata:
                 _self = "Section"
                 parent = "Title"
                 grandparent = None
+                current_document.metadata["level"] = 2
 
             # <H1/> (already set)
-            elif "Title" in doc_split.metadata:
+            elif "Title" in current_document.metadata:
                 continue
 
             # Otherwise, error
             else:
-                raise ValueError(f"Metadata headers should be one of Union[Title, Section, Subsection, Detailed Subsection]. Got `{doc_split.metadata}` instead.")
+                raise ValueError(
+                    f"Metadata headers should be one of Union[Title, Section, Subsection, Detailed Subsection]. Got `{current_document.metadata}` instead.")
 
-            # Enhance doc_split metadata
-            doc_split.metadata["id"] = doc_split.metadata.get(_self)
-            doc_split.metadata["summary"] = None
-            
-            # Add doc_split to V set
-            self.V[doc_split.metadata["id"]] = doc_split
+            # Enhance metadata
+            current_document.metadata["id"] = current_document.metadata[_self]
+            current_document.metadata["summary"] = None
 
-            # Add parent if not exists
-            if (parent_id := doc_split.metadata.get(parent)) not in self.V:
-                self.V[parent_id] = Document(
-                    page_content="This is a high-level vertex",
+            # update V
+            self.V[current_document.metadata["id"]] = current_document
+
+            # Add parent if not exists (i.e. parent is a no content node)
+            # Note: the Langchain MD splitter ignores headings that contain no content below
+            if (parent_id := current_document.metadata.get(parent)) not in self.V:
+                parent_document = Document(
+                    page_content="",
                     metadata={
-                        "children": [],
-                        "summary": None,
+                        # Parent must be at least <H2>...
                         "id": parent_id,
-                        "Title": self.root.metadata.get("Title"),
-                        "Section": doc_split.metadata.get("Section"),
-                        _self == "Detailed Subsection" and "Subsection": doc_split.metadata.get("Subsection"),
+                        "summary": None,
+                        "children": [],
+                        "parent": current_document.metadata["Title"],
+                        "Title": current_document.metadata["Title"],
+                        "Section": current_document.metadata["Section"],
                     }
                 )
+                
+                # ... and at most <H3>
+                if current_document.metadata['level'] == 4:
+                    parent_document.metadata["parent"] = current_document.metadata["Section"],
+                    parent_document.metadata['Subsection'] = current_document.metadata['Subsection']
+
+                # Add parent to V
+                self.V[parent_id] = parent_document
 
                 # Update grandparent
-                if (grandparent_id := doc_split.metadata.get(grandparent)):
-                    self.V[grandparent_id].metadata['children'].append(self.V[parent_id].metadata['id'])
-            
+                if (grandparent_id := current_document.metadata.get(grandparent)):
+                    self.V[grandparent_id].metadata['children'].append(
+                        self.V[parent_id].metadata['id']
+                    )
+
             # Update parent
-            self.V[parent_id].metadata['children'].append(doc_split.metadata['id'])
+            self.V[parent_id].metadata["level"] = current_document.metadata['level'] - 1
+            self.V[parent_id].metadata['children'].append(
+                current_document.metadata['id']
+            )
 
     def create_summaries_recursively(self, agent: Runnable):
         def _run_recursion(curr=self.root, agent=agent):
@@ -116,11 +137,16 @@ class MarkdownTree:
             # Recursion
             for child in curr.metadata['children']:
                 _run_recursion(curr=self.V[child], agent=agent)
-            
-            # Generate parent summary
-            summaries = [self.V[child].metadata['summary'] for child in curr.metadata['children'] if self.V[child].metadata.get('summary')]
+
+            # Generate parent summary...
+            summaries = [
+                self.V[child].metadata['summary']
+                for child in curr.metadata['children']
+                if self.V[child].metadata.get('summary')
+            ]
+
             curr.metadata['summary'] = agent.invoke({
-                'input': StringParser.from_array(summaries)
+                'input': StringParser.from_array([curr.page_content] + summaries)
             })
 
         _run_recursion()
